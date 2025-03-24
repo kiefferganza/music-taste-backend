0b04547fd34616df9db3dd2ce438dff87521b2e1
Kieffer Ganza <kiefferganza23@gmail.com>
2025-03-25 01:05:55 +0800

    auth routes

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
     Route::get('albums', [\App\Http\Controllers\AlbumController::class,'index']);
     Route::post('/album/{album}/vote', [\App\Http\Controllers\VoteController::class, 'store']);
     Route::delete('/album/{album}', [\App\Http\Controllers\AlbumController::class, 'destroy'])->middleware('can:delete,album');
